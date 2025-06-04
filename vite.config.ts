c0a4d577daec38579377be90f76882c0004060a4
nas11ai <kairupannasai@gmail.com>
2025-06-04 21:10:22 +0800

    feat: init auto route

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
+import VueRouter from "unplugin-vue-router/vite";
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [VueRouter({}), vue(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
