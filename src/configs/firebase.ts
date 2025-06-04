bd92cc4ef8f36d5627465a6e8bcdc50cd1a0296c
nas11ai <kairupannasai@gmail.com>
2025-06-04 20:34:07 +0800

    feat: init firebase config

---
+++ src/configs/firebase.ts
@@ -0,0 +1,19 @@
+// lib/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+// Export references to the Firestore
+export const usersRef = collection(db, "users");
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { VueFire, VueFireAuth } from "vuefire";
+import "./index.css";
+import App from "./App.vue";
+import { firebaseApp } from "./configs/firebase";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(VueFire, {
+  firebaseApp,
+  modules: [VueFireAuth()],
+});
+
+app.mount("#app");
